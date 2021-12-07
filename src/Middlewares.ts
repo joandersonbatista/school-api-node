b2144b13bed7168db9bd44091c5a68122c2673c7
Joanderson Batista <joandersonbatista.br@gmail.com>
2021-12-07 20:03:59 -0300

    add cors and helmet :memo:

--- src/Middlewares.ts
+++ src/Middlewares.ts
@@ -1,4 +1,6 @@
 import { urlencoded, json, Application } from "express";
+import cors from "cors";
+import helmet from "helmet";
 
 import { IMiddlewaresProtocol } from "./IMiddlewaresProtocol";
 
@@ -10,6 +12,8 @@ class Middlewares implements IMiddlewaresProtocol {
   middlewares(): void {
     this.app.use(urlencoded({ extended: true }));
     this.app.use(json());
+    this.app.use(cors());
+    this.app.use(helmet());
   }
 }
 
