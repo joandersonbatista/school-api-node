f07b7252996c5da5e327c081a14f6fe0835542bc
Joanderson Batista <joandersonbatista.br@gmail.com>
2021-12-15 15:34:29 -0300

    refactor and add return student when updating :hammer:

--- src/useCases/students/UpdateStudent/IUpdateStudent.ts
+++ src/useCases/students/UpdateStudent/IUpdateStudent.ts
@@ -1,7 +1,14 @@
+import { IStudentsAttributes } from "../../../models/IStudentsAttributes";
+import { IUpdateProfilePictureDTO } from "../../profilePicture/updateProfilePicture/IUpdateProfilePictureDTO";
 import { IUpdateStudentDTO } from "./IUpdateStudentDTO";
 
+type CreateProfilePictureType = Omit<Express.Multer.File, "stream" | "buffer">
+
 interface IUpdateStudent {
-  execute(student: IUpdateStudentDTO, picture?: Express.Multer.File): Promise<void>;
+  execute(
+    student: IUpdateStudentDTO,
+    picture?: CreateProfilePictureType | IUpdateProfilePictureDTO,
+  ): Promise<IStudentsAttributes>
 }
 
-export { IUpdateStudent };
+export { IUpdateStudent, CreateProfilePictureType };
